a3b3f6d46d1e7df4cb9a72fc20689cf5a877e63c
WarwickSeven <WarwickSeven@gmail.com>
2022-02-28 17:52:13 +0300

    Домашнее задание 5-3

--- Warwick_hm53/main.cpp
+++ Warwick_hm53/main.cpp
@@ -1,14 +1,44 @@
-//
-//  main.cpp
-//  Warwick_hm53
-//
-//  Created by Александр Тихонов on 28.02.2022.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using std::cout;
+using std::endl;
+
+// домашнее задание 5-3
+
+bool PrintArray(int arr[], int size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return true;
+}
+
+bool initArray(int arr[], int size)
+{
+    int mod = 1;
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = mod;
+        mod += 3;
+    }
+    cout << endl;
+    return true;
+}
+int main()
+{
+    const int mysize = 8;
+    int myArr[] = { };
+        
+    if (initArray(myArr, mysize))
+    {
+        cout << "Array was sucesfully modified\n" << endl;
+    }
+    
+    if (PrintArray(myArr, mysize))
+    {
+        cout << "Array after modification" << endl;
+    }
     return 0;
 }
